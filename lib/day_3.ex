77fd3d33bc670d66fc09a437185c16226d8706e1
Joel Darling <joel.darling@gmail.com>
2018-12-04 07:57:47 -0500

    finish day 3

--- lib/day_3.ex
+++ lib/day_3.ex
@@ -2,26 +2,29 @@ defmodule Aoc2018.Day3 do
   def start do
 
     char_map = Aoc2018.file_to_list("day_3_input.txt")
-    |> Enum.map(&map_claim(&1))
-
-    [first, second | rest] = char_map
-    initial_val = MapSet.intersection(first, second)
-    Enum.reduce(rest, initial_val, fn curr, acc ->
-      overlap = MapSet.intersection(acc, curr)
-      if MapSet.size(overlap) > 0 do MapSet.put(acc, overlap) else acc end
-    end)
-    |> Enum.count
+    |> Enum.flat_map(&map_claim(&1))
+    |> get_dupes
+    |> MapSet.new
+    |> MapSet.size
+    
   end
 
-  def merge_sets(input) do
-    
+  def get_dupes(list) do
+    acc_dupes = fn(x, {elems, dupes}) ->
+      case Map.has_key?(elems, x) do
+        true -> {elems, Map.put(dupes, x, nil)}
+        false -> {Map.put(elems, x, nil), dupes}
+      end
+    end
+
+    list |> Enum.reduce({%{}, %{}}, acc_dupes) |> elem(1) |> Map.to_list
   end
 
   def map_claim(input) do
     [w, h] = get_dimenions(input)
     [x, y] = get_coords(input)
     
-    for dx <- x..x + (w - 1), dy <- y..y + (h - 1), into: %MapSet{}, do: {dx, dy}
+    for dx <- x..x + (w - 1), dy <- y..y + (h - 1), into: [], do: {dx, dy}
   end
 
   def get_dimenions(str) do
